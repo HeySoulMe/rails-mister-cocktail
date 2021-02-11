51d349eafc16dd6f60560ab9956d53cb277df129
Edouard Belfond <edouard.belfond@grenoble-em.com>
2021-02-11 16:52:24 +0100

    exo2 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses
+  end
+
+  resources :doses, only: [:destroy]
+
+ # resources :restaurants, only: [ :index, :create, :show, :new ] do
+ #    resources :reviews, only: [ :new, :create ]
+
+    # resources :cocktails, only: [ :index, :create, :show, :new ] do
+    #   resources :ingredient, only: [ :new, :create ],
+    # end
+    # resources :cocktails, only: [ :index, :create, :show, :new ] do
+    #   resources :doses, only: [ :new, :create ]
+    # end
 end
